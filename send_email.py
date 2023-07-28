f5a5ea36eb2cc0180d0734224de4939aa54bd16c
SilviuAlexandru <119167365+SilviuAlexandru@users.noreply.github.com>
2023-07-28 12:45:06 +0300

    Add files via upload

---
+++ send_email.py
@@ -0,0 +1,20 @@
+import smtplib
+
+MY_EMAIL = "silviupython99@gmail.com"
+APP_PASSWORD = "lkrdksqiunoonlpo"
+
+class SendEmail:
+    def __init__(self, email_to_send, password_to_generate):
+        self.email = email_to_send
+        self.password = password_to_generate
+
+    def send_email(self):
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=APP_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=self.email,
+                msg=f"Subject:Change password\n\nYour new password is: {self.password}"
+            )
+        connection.close()
